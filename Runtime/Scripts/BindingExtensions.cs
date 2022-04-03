f9abbc5fc4482387c3e34ec84d242067cfbe5068
Jackson Levitt <47047592+crazyjackel@users.noreply.github.com>
2022-04-03 15:02:27 -0400

    Update BindingExtensions.cs

--- Runtime/Scripts/BindingExtensions.cs
+++ Runtime/Scripts/BindingExtensions.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -7,22 +8,52 @@ using UnityEngine.UIElements;
 
 namespace RedMoon.ReactiveKit
 {
-    public static class BindingExtensions
+    public static partial class BindingExtensions
     {
         #region Visual Element
-        public static IDisposable BindEnabled(this VisualElement element, IReadOnlyReactiveProperty<bool> property)
+        /// <summary>
+        /// Binds Element's Enabled State to Boolean Observable
+        /// </summary>
+        /// <param name="element">Element to Bind</param>
+        /// <param name="observable">Observable Stream to Subscribe to</param>
+        /// <returns>Disposable for Disposing Subscription</returns>
+        [MustUseReturnValue]
+        public static IDisposable BindEnabled(this VisualElement element, IObservable<bool> observable)
         {
-            return property.SubscribeWithState(element, (__property, __element) =>
-            {
-                element.SetEnabled(__property);
-            });
+            return observable.ObserveOnMainThread().DistinctUntilChanged().Subscribe(element.SetEnabled);
         }
+        /// <summary>
+        /// Binds Element's Visibility State to Visibility Observable
+        /// </summary>
+        /// <param name="element">Element to Bind</param>
+        /// <param name="observable">Observable Stream to Subscribe to</param>
+        /// <returns>Disposable for Disposing Subscription</returns>
+        [MustUseReturnValue]
+        public static IDisposable BindVisibility(this VisualElement element, IObservable<Visibility> observable)
+        {
+            return observable.ObserveOnMainThread().DistinctUntilChanged().Subscribe(value => element.style.visibility = value);
+        }
+        /// <summary>
+        /// Bind Element's Click Callback to Command
+        /// </summary>
+        /// <param name="element">Element to Bind</param>
+        /// <param name="command">Command to Execute</param>
+        /// <returns>Disposable for Disposing Subscription</returns>
+        [MustUseReturnValue]
         public static IDisposable BindClick(this VisualElement element, IReactiveCommand<ClickEvent> command)
         {
-            var d1 = element.BindEnabled(command.CanExecute);
-            var d2 = element.BindCallback(command);
+            IDisposable d1 = element.BindEnabled(command.CanExecute);
+            IDisposable d2 = element.BindCallback(command);
             return StableCompositeDisposable.Create(d1, d2);
         }
+        /// <summary>
+        /// Bind Element's Click Callback to Command
+        /// </summary>
+        /// <param name="element">Element to Bind</param>
+        /// <param name="command">Command to Execute</param>
+        /// <param name="dataForCallback">Callback as a part of Command</param>
+        /// <returns>Disposable for Disposing Subscription</returns>
+        [MustUseReturnValue]
         public static IDisposable BindClick<TArgs>(this VisualElement element, IReactiveCommand<(ClickEvent, TArgs)> command, TArgs dataForCallback)
         {
             var d1 = element.BindEnabled(command.CanExecute);
@@ -32,95 +63,74 @@ namespace RedMoon.ReactiveKit
         #endregion
 
         #region Callback Event Handler
+        [MustUseReturnValue]
         public static IDisposable BindCallback<TEventType>(this CallbackEventHandler element, EventCallback<TEventType> callback, TrickleDown trickleDown = TrickleDown.NoTrickleDown) where TEventType : EventBase<TEventType>, new()
         {
             element.RegisterCallback(callback, trickleDown);
             return Disposable.Create(() => { element.UnregisterCallback(callback, trickleDown); });
         }
+        [MustUseReturnValue]
         public static IDisposable BindCallback<TEventType, TUserArgsType>(this CallbackEventHandler element, EventCallback<TEventType, TUserArgsType> callback, TUserArgsType dataForCallback, TrickleDown trickleDown = TrickleDown.NoTrickleDown) where TEventType : EventBase<TEventType>, new()
         {
             element.RegisterCallback(callback, dataForCallback, trickleDown);
             return Disposable.Create(() => { element.UnregisterCallback(callback, trickleDown); });
         }
+        [MustUseReturnValue]
         public static IDisposable BindCallback<TEventType>(this CallbackEventHandler element, IReactiveCommand<TEventType> command, TrickleDown trickleDown = TrickleDown.NoTrickleDown) where TEventType : EventBase<TEventType>, new()
         {
-            var callback = new EventCallback<TEventType>((ev) => command.Execute(ev));
+            EventCallback<TEventType> callback = new EventCallback<TEventType>((ev) => command.Execute(ev));
             element.RegisterCallback(callback, trickleDown);
             return Disposable.Create(() => { element.UnregisterCallback(callback, trickleDown); });
         }
+        [MustUseReturnValue]
         public static IDisposable BindCallback<TEventType, TUserArgsType>(this CallbackEventHandler element, IReactiveCommand<(TEventType, TUserArgsType)> command, TUserArgsType dataForCallback, TrickleDown trickleDown = TrickleDown.NoTrickleDown) where TEventType : EventBase<TEventType>, new()
         {
-            var callback = new EventCallback<TEventType, TUserArgsType>((ev, args) => command.Execute((ev, args)));
+            EventCallback<TEventType, TUserArgsType> callback = new EventCallback<TEventType, TUserArgsType>((ev, args) => command.Execute((ev, args)));
             element.RegisterCallback(callback, dataForCallback, trickleDown);
             return Disposable.Create(() => { element.UnregisterCallback(callback, trickleDown); });
         }
         #endregion
 
         #region Notifications
+        [MustUseReturnValue]
         public static IDisposable BindValueChanged<T>(this INotifyValueChanged<T> element, IReactiveCommand<ChangeEvent<T>> command)
         {
-            var callback = new EventCallback<ChangeEvent<T>>((ev) => command.Execute(ev));
-            element.RegisterValueChangedCallback(callback);
-            return Disposable.Create(() => { element.UnregisterValueChangedCallback(callback); });
-        }
-        public static IDisposable BindValueChanged<T>(this INotifyValueChanged<T> element, IReactiveProperty<T> property)
-        {
-            var callback = new EventCallback<ChangeEvent<T>>((ev) => property.Value = ev.newValue);
+            EventCallback<ChangeEvent<T>> callback = new EventCallback<ChangeEvent<T>>((ev) => command.Execute(ev));
             element.RegisterValueChangedCallback(callback);
             return Disposable.Create(() => { element.UnregisterValueChangedCallback(callback); });
         }
         /// <summary>
-        /// Binds Element changes to Update Property Values. 
-        /// Then Initializes Values.
+        /// Binds a Property Value to Element Changing
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="element">Element with Value Changed Callbacks</param>
-        /// <param name="property">Reactive Property that value should change on a Value Changed Callback</param>
-        /// <param name="stateIsProperty">Whether Default Value should be Default Property Value</param>
-        /// <returns></returns>
-        public static IDisposable BindValueChangedWithState<T>(this INotifyValueChanged<T> element, IReactiveProperty<T> property, bool stateIsProperty = true)
+        /// <typeparam name="T">Type for Change Event</typeparam>
+        /// <param name="element">Element that Notifies Change</param>
+        /// <param name="property">Property to Change on Notification</param>
+        /// <returns>Disposable for Disposing Subscription</returns>
+        [MustUseReturnValue]
+        public static IDisposable BindValueChanged<T>(this INotifyValueChanged<T> element, IReactiveProperty<T> property)
         {
-            var d1 = BindValueChanged(element, property);
-            if (stateIsProperty)
-            {
-                element.value = property.Value;
-            }
-            else
-            {
-                property.Value = element.value;
-            }
-            return d1;
+            EventCallback<ChangeEvent<T>> callback = new EventCallback<ChangeEvent<T>>((ev) => property.Value = ev.newValue);
+            element.RegisterValueChangedCallback(callback);
+            return Disposable.Create(() => { element.UnregisterValueChangedCallback(callback); });
         }
-        public static IDisposable BindToValueChanged<T>(this INotifyValueChanged<T> element, IReactiveProperty<T> property)
+        /// <summary>
+        /// Binds an Element to a Property Changing
+        /// Does not Trigger Element Notifications
+        /// </summary>
+        /// <typeparam name="T">Type for Fields</typeparam>
+        /// <param name="element">Element that has value updated</param>
+        /// <param name="observable">Observable to update value</param>
+        /// <returns>Disposable for Disposing Subscription</returns>
+        [MustUseReturnValue]
+        public static IDisposable BindToValueChanged<T>(this INotifyValueChanged<T> element, IObservable<T> observable)
         {
-            return property.SubscribeWithState(element, (__property, __element) =>
-            {
-                __element.SetValueWithoutNotify(__property);
-            });
+            return observable.ObserveOnMainThread().DistinctUntilChanged().Subscribe((ev) => element.SetValueWithoutNotify(ev));
         }
-        public static IDisposable BindToValueChangedWithState<T>(this INotifyValueChanged<T> element, IReactiveProperty<T> property, bool stateIsProperty = true)
-        {
-            var d1 = BindToValueChanged(element, property);
-            if (stateIsProperty)
-            {
-                element.value = property.Value;
-            }
-            else
-            {
-                property.Value = element.value;
-            }
-            return d1;
-        }
+        [MustUseReturnValue]
         public static IDisposable BindTwoWayValueChanged<T>(this INotifyValueChanged<T> element, IReactiveProperty<T> property)
         {
-            var d1 = BindValueChanged(element, property);
-            var d2 = BindToValueChanged(element, property);
-            return StableCompositeDisposable.Create(d1, d2);
-        }
-        public static IDisposable BindTwoWayValueChangedWithState<T>(this INotifyValueChanged<T> element, IReactiveProperty<T> property, bool stateIsProperty = true)
-        {
-            var d1 = BindValueChangedWithState(element, property, stateIsProperty);
-            var d2 = BindToValueChangedWithState(element, property, stateIsProperty);
+            IDisposable d1 = BindValueChanged(element, property);
+            IDisposable d2 = BindToValueChanged(element, property);
             return StableCompositeDisposable.Create(d1, d2);
         }
         #endregion
